f00c56da8436c6c65cf083577efc0fc8ba4f5485
James Coliz <jcoliz@outlook.com>
2025-05-12 10:44:48 -0700

    feat: Add Log Ingestion client

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sample logs generator for Jamf Protect
+
+## Getting started
+
+```
+.\.azure\deploy\Deploy-Services.ps1 -ResourceGroup sentinel-jamf -Location westus
+```
--- WorkerApp/Program.cs
+++ WorkerApp/Program.cs
@@ -1,6 +1,35 @@
+using Azure.Identity;
+using Microsoft.Extensions.Azure;
 using WorkerApp;
+using WorkerApp.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddTomlFile("config.toml", optional: true, reloadOnChange: true);
+
+builder.Services.Configure<LogIngestionOptions>(
+    builder.Configuration.GetSection(LogIngestionOptions.Section)
+);
+builder.Services.Configure<IdentityOptions>(
+    builder.Configuration.GetSection(LogIngestionOptions.Section)
+);
+
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    // Create a client secret credential using supplied credentials
+    IdentityOptions identityOptions = new();
+    builder.Configuration.Bind(IdentityOptions.Section, identityOptions);
+    var token = new ClientSecretCredential(identityOptions.TenantId.ToString(), identityOptions.AppId.ToString(), identityOptions.AppSecret);
+
+    // Add a log ingestion client, using endpoint from configuration
+    LogIngestionOptions logOptions = new();
+    builder.Configuration.Bind(LogIngestionOptions.Section, logOptions);
+    clientBuilder.AddLogsIngestionClient(logOptions.EndpointUri);
+
+    // Add the desired Azure credential to the client
+    clientBuilder.UseCredential(token);
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
